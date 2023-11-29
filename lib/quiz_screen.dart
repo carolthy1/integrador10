b003eaf5e6fd4d242e8186d123e5ffbb7dfdc2a6
carolthy1 <thauanycarolinecurso@gmail.com>
2023-11-29 17:33:14 -0300

    mais perguntas no quiz sobre pomodoro (tem q arrumar perguntas melhores e melhorar a pontuação do quiz))

--- lib/quiz_screen.dart
+++ lib/quiz_screen.dart
@@ -37,6 +37,21 @@ class _QuizScreenState extends State<QuizScreen> {
   int questionIndex = 0;
   List<String> answers = [];
   bool showResults = false;
+  int scorePomodoro = 0;
+
+  void answerQuestion(String answer) {
+    setState(() {
+      answers.add(answer);
+
+      if (questionIndex < questions.length - 1) {
+        questionIndex++;
+      } else {
+        // Se todas as perguntas foram respondidas, mostra os resultados.
+        showResults = true;
+        saveUserPreferences(); // Salva as respostas do usuário
+      }
+    });
+  }
 
   final List<Map<String, dynamic>> questions = [
     {
@@ -66,34 +81,68 @@ class _QuizScreenState extends State<QuizScreen> {
         'Não, prefiro flexibilidade'
       ],
     },
+    {
+      'questionText': 'Prefere estudos contínuos ou intervalados?',
+      'answers': ['Contínuos', 'Intervalados'],
+    },
+    {
+      'questionText':
+          'Você costuma utilizar técnicas de gerenciamento de tempo?',
+      'answers': ['Sim', 'Não'],
+    },
+    {
+      'questionText':
+          'Quanto tempo você consegue se concentrar sem interrupções?',
+      'answers': ['Curto período', 'Longo período'],
+    },
+    // Novas perguntas relacionadas ao método de estudo "Pomodoro"
+    {
+      'questionText': 'Você costuma utilizar a técnica Pomodoro?',
+      'answers': ['Sim', 'Não'],
+    },
+    {
+      'questionText': 'Qual é a duração ideal para seus períodos de estudo?',
+      'answers': ['25 minutos', '50 minutos'],
+    },
+    {
+      'questionText': 'Prefere pausas curtas ou longas durante os intervalos?',
+      'answers': ['Curtas', 'Longas'],
+    },
+    {
+      'questionText':
+          'Você prefere trabalhar em blocos de tempo focado seguidos por pausas curtas?',
+      'answers': ['Sim', 'Não'],
+    },
+    {
+      'questionText': 'Como você lida com interrupções durante o trabalho?',
+      'answers': [
+        'Bem, consigo retomar facilmente',
+        'Prefiro evitar interrupções'
+      ],
+    },
+    {
+      'questionText':
+          'Você acha útil definir metas de produtividade para períodos específicos?',
+      'answers': ['Sim', 'Não'],
+    },
+    {
+      'questionText':
+          'Qual a sua opinião sobre pausas curtas durante o estudo/trabalho?',
+      'answers': [
+        'São essenciais para minha produtividade',
+        'Podem ser dispensáveis'
+      ],
+    },
+    {
+      'questionText':
+          'Você se considera uma pessoa concentrada por longos períodos?',
+      'answers': ['Sim', 'Não'],
+    },
   ];
-
-  void answerQuestion(String answer) {
-    answers.add(answer);
-
-    if (questionIndex < questions.length - 1) {
-      setState(() {
-        questionIndex++;
-      });
-    } else {
-      saveUserPreferences();
-      setState(() {
-        showResults = true;
-      });
-    }
-  }
 
   Future<void> saveUserPreferences() async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setStringList('userAnswers', answers);
-  }
-
-  void restartTest() {
-    setState(() {
-      questionIndex = 0;
-      answers = [];
-      showResults = false;
-    });
   }
 
   String calculateRecommendedMethodology() {
@@ -101,6 +150,7 @@ class _QuizScreenState extends State<QuizScreen> {
     int scoreScrum = 0;
     int scoreKanban = 0;
     int scoreScrumban = 0;
+    int scorePomodoro = 0;
 
     for (String answer in answers) {
       if (answer == 'Detalhado') {
@@ -133,9 +183,46 @@ class _QuizScreenState extends State<QuizScreen> {
       } else if (answer == 'Não, prefiro flexibilidade') {
         scoreScrumban++;
       }
+      // Pontuação relacionada à técnica Pomodoro
+      if (answer == 'Sim') {
+        scorePomodoro++;
+      } else if (answer == 'Não') {
+        // Pode adicionar lógica adicional, se necessário
+      }
+      if (answer == '25 minutos') {
+        scorePomodoro++;
+      } else if (answer == '50 minutos') {
+        // Pode adicionar lógica adicional, se necessário
+      }
+      if (answer == 'Curtas') {
+        scorePomodoro++;
+      } else if (answer == 'Longas') {
+        // Pode adicionar lógica adicional, se necessário
+      }
+      if (answer == 'Sim') {
+        scorePomodoro++;
+      } else if (answer == 'Não') {
+        // Pode adicionar lógica adicional, se necessário
+      }
+      if (answer == 'Bem, consigo retomar facilmente') {
+        scorePomodoro++;
+      } else if (answer == 'Prefiro evitar interrupções') {
+        // Pode adicionar lógica adicional, se necessário
+      }
+      if (answer == 'Sim') {
+        scorePomodoro++;
+      } else if (answer == 'Não') {
+        // Pode adicionar lógica adicional, se necessário
+      }
     }
 
-    if (scoreScrum > scoreCascata &&
+    // Adiciona a pontuação do Pomodoro à lógica de pontuação geral
+    if (scorePomodoro > scoreScrum &&
+        scorePomodoro > scoreCascata &&
+        scorePomodoro > scoreKanban &&
+        scorePomodoro > scoreScrumban) {
+      return 'Pomodoro';
+    } else if (scoreScrum > scoreCascata &&
         scoreScrum > scoreKanban &&
         scoreScrum > scoreScrumban) {
       return 'Scrum';
@@ -158,6 +245,8 @@ class _QuizScreenState extends State<QuizScreen> {
         return 'Kanban é uma abordagem visual que se concentra na gestão do fluxo de trabalho. É recomendada para projetos que exigem um controle rigoroso do trabalho em andamento.';
       case 'Scrumban':
         return 'Scrumban combina elementos do Scrum e do Kanban, oferecendo flexibilidade e controle do fluxo. É uma escolha sólida para projetos que desejam uma abordagem híbrida.';
+      case 'Pomodoro':
+        return 'A técnica Pomodoro é um método de gerenciamento de tempo que utiliza intervalos de trabalho focado, chamados "Pomodoros", seguidos por breves pausas. Essa abordagem visa melhorar a eficiência e manter alta a concentração ao dividir o trabalho em segmentos cronometrados.';
       default:
         return 'Descrição não disponível';
     }
@@ -173,9 +262,19 @@ class _QuizScreenState extends State<QuizScreen> {
         return '\n1. Visualize seu fluxo de trabalho com um quadro Kanban.\n2. Limite o trabalho em andamento para evitar sobrecarga.\n3. Otimize continuamente seu processo com base nos dados do quadro.';
       case 'Scrumban':
         return '\n1. Combine elementos do Scrum e do Kanban conforme necessário.\n2. Mantenha um equilíbrio entre flexibilidade e controle do fluxo.\n3. Ajuste suas práticas de acordo com as necessidades específicas do projeto.';
+      case 'Pomodoro':
+        return '\n1. Configure um temporizador para períodos de trabalho de 25 minutos (um Pomodoro).\n2. Concentre-se intensamente na tarefa durante o Pomodoro, evitando distrações.\n3. Após cada Pomodoro, faça uma pausa curta de 5 minutos.\n4. Após completar quatro Pomodoros, faça uma pausa mais longa, de 15-30 minutos.\n5. Ajuste a duração dos Pomodoros e pausas conforme sua preferência e necessidades.';
       default:
         return 'Dicas não disponíveis';
     }
+  }
+
+  void restartCallback() {
+    setState(() {
+      questionIndex = 0;
+      answers = [];
+      showResults = false;
+    });
   }
 
   @override
@@ -183,7 +282,7 @@ class _QuizScreenState extends State<QuizScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Recomenda Metodologias'),
-        backgroundColor: Color(0xFFDDC3F5), // Mudei a cor da barra de navegação
+        backgroundColor: Color(0xFFDDC3F5),
       ),
       body: showResults
           ? ResultsWidget(
@@ -192,7 +291,7 @@ class _QuizScreenState extends State<QuizScreen> {
                   getMethodologyDescription(calculateRecommendedMethodology()),
               methodologyTips:
                   getMethodologyTips(calculateRecommendedMethodology()),
-              restartCallback: restartTest,
+              restartCallback: restartCallback,
             )
           : QuestionWidget(
               questionText: questions[questionIndex]['questionText'],
@@ -290,77 +389,81 @@ class ResultsWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Card(
-        elevation: 8,
-        margin: EdgeInsets.all(16),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Metodologia recomendada para você: $recommendedMethodology',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontFamily: 'OpenSans',
-                  color: Colors.black, // Mudei a cor do texto para preto
-                ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Descrição:',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontFamily: 'OpenSans',
-                  color: Colors.black, // Mudei a cor do texto para preto
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  methodologyDescription,
+      child: SingleChildScrollView(
+        child: Card(
+          elevation: 8,
+          margin: EdgeInsets.all(16),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Metodologia recomendada para você: $recommendedMethodology',
                   style: TextStyle(
-                      fontSize: 18,
-                      fontFamily: 'OpenSans',
-                      color: Colors.black), // Mudei a cor do texto para preto
-                  textAlign: TextAlign.justify, // Adicionei a justificação
+                    fontSize: 24,
+                    fontFamily: 'OpenSans',
+                    color: Colors.black, // Mudei a cor do texto para preto
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Dicas práticas:',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontFamily: 'OpenSans',
-                  color: Colors.black, // Mudei a cor do texto para preto
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  methodologyTips,
+                SizedBox(height: 20),
+                Text(
+                  'Descrição:',
                   style: TextStyle(
-                      fontSize: 18,
-                      fontFamily: 'OpenSans',
-                      color: Colors.black), // Mudei a cor do texto para preto
-                  textAlign: TextAlign.justify, // Adicionei a justificação
-                ),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: restartCallback,
-                style: ElevatedButton.styleFrom(
-                  primary: Color(0xFFDDC3F5), // Mudei a cor do botão
-                  onPrimary: Colors.black, // Mudei a cor do texto para preto
-                  padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
+                    fontSize: 20,
+                    fontFamily: 'OpenSans',
+                    color: Colors.black, // Mudei a cor do texto para preto
                   ),
                 ),
-                child: Text('Reiniciar Teste'),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    methodologyDescription,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontFamily: 'OpenSans',
+                      color: Colors.black, // Mudei a cor do texto para preto
+                    ),
+                    textAlign: TextAlign.justify, // Adicionei a justificação
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Dicas práticas:',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'OpenSans',
+                    color: Colors.black, // Mudei a cor do texto para preto
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    methodologyTips,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontFamily: 'OpenSans',
+                      color: Colors.black, // Mudei a cor do texto para preto
+                    ),
+                    textAlign: TextAlign.justify, // Adicionei a justificação
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: restartCallback,
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xFFDDC3F5), // Mudei a cor do botão
+                    onPrimary: Colors.black, // Mudei a cor do texto para preto
+                    padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                  child: Text('Reiniciar Teste'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
