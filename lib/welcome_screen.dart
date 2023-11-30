bb74480b2cd3d5f8feaa96b37f09f216c907d5e9
carolthy1 <thauanycarolinecurso@gmail.com>
2023-11-30 16:33:02 -0300

    botoes "arrumados" na tela de welcome

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'registration_screen.dart';
 import 'login_screen.dart';
 
@@ -10,8 +9,8 @@ class WelcomeScreen extends StatelessWidget {
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            begin: Alignment.centerLeft, // Começa da esquerda
-            end: Alignment.centerRight, // Termina na direita
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
             colors: [
               Color.fromARGB(255, 228, 203, 231),
               Color.fromARGB(255, 203, 176, 243),
@@ -26,28 +25,23 @@ class WelcomeScreen extends StatelessWidget {
                   mainAxisSize: MainAxisSize.min,
                   children: <Widget>[
                     ClipRRect(
-                      borderRadius:
-                          BorderRadius.circular(20), // Borda arredondada
+                      borderRadius: BorderRadius.circular(20),
                       child: Image.asset(
                         'assets/study_wave.png',
                         width: 150,
                         height: 150,
                       ),
-                    ), // Substitua pelo caminho da sua imagem
-
-                    SizedBox(height: 30), // Espaço entre a imagem e o texto
-
+                    ),
+                    SizedBox(height: 30),
                     Text(
-                      'Study Wave', // Nome do seu aplicativo
+                      'Study Wave',
                       style: TextStyle(
                         fontFamily: 'MinhaFonte',
                         fontSize: 30,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-
-                    SizedBox(height: 10), // Espaço entre o título e a descrição
-
+                    SizedBox(height: 10),
                     Text(
                       'Sua melhor plataforma de estudo.',
                       style: TextStyle(fontFamily: 'OpenSans', fontSize: 15),
@@ -56,75 +50,77 @@ class WelcomeScreen extends StatelessWidget {
                 ),
               ),
             ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => RegistrationScreen()),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Color.fromARGB(
-                          228, 184, 119, 202), // Cor personalizada
-                      minimumSize: Size(400, 40), // Tamanho do botão
-                      shape: RoundedRectangleBorder(
-                        borderRadius:
-                            BorderRadius.circular(10), // Borda arredondada
+            Padding(
+              padding: const EdgeInsets.only(
+                  bottom: 100.0), // Ajuste o valor conforme necessário
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => RegistrationScreen(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Color.fromARGB(255, 221, 195, 245),
+                        minimumSize: Size(300, 40),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        side: BorderSide(
+                          color: Color.fromARGB(255, 112, 112, 112),
+                          width: 0.5,
+                        ),
+                        elevation: 5,
                       ),
-                      side: BorderSide(
-                        color: Color.fromARGB(255, 0, 0, 0), // Cor da borda
-                        width: 1.0, // Largura da borda
-                      ),
-                    ),
-                    child: Text(
-                      'Começar agora',
-                      style: TextStyle(
-                        fontFamily: 'OpenSans',
-                        fontSize: 17,
-                        color: Color.fromARGB(255, 0, 0, 0), // Cor do texto
-                      ),
-                    ),
-                  ),
-
-                  SizedBox(height: 5), // Espaço entre os botões
-
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => LoginScreen()),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Color.fromARGB(
-                          186, 151, 110, 183), // Cor personalizada
-                      minimumSize: Size(400, 40), // Tamanho do botão
-                      shape: RoundedRectangleBorder(
-                        borderRadius:
-                            BorderRadius.circular(10), // Borda arredondada
-                      ),
-                      side: BorderSide(
-                        color: Color.fromARGB(255, 0, 0, 0), // Cor da borda
-                        width: 1.0, // Largura da borda
+                      child: Text(
+                        'Começar agora',
+                        style: TextStyle(
+                            fontFamily: 'OpenSans',
+                            fontSize: 15,
+                            color: Color.fromARGB(255, 0, 0, 0)),
                       ),
                     ),
-                    child: Text(
-                      'Já tenho uma conta',
-                      style: TextStyle(
-                        fontFamily: 'OpenSans',
-                        fontSize: 17,
-                        color: Color.fromARGB(255, 0, 0, 0), // Cor do texto
+                    SizedBox(height: 5),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LoginScreen(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Color.fromARGB(255, 221, 195, 245),
+                        minimumSize: Size(300, 40),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        side: BorderSide(
+                          color: Color.fromARGB(255, 112, 112, 112),
+                          width: 0.5,
+                        ),
+                        elevation: 5,
+                      ),
+                      child: Text(
+                        'Já tenho uma conta',
+                        style: TextStyle(
+                          fontFamily: 'OpenSans',
+                          fontSize: 14,
+                          color: Color.fromARGB(255, 0, 0, 0),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 15),
-                ],
+                    SizedBox(height: 15),
+                  ],
+                ),
               ),
             ),
           ],
